6a39ffb03af9c6fbc21a3f64f6fb5104a0c30983
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-04-05 21:41:43 +0500

    practice

--- script.js
+++ script.js
@@ -1,8 +1,81 @@
-let data = "Hello ";
-let data2 = "Students";
+// let data = "Hello ";
+// let data2 = "Students";
 
-let data3 = data + data2;
+// let data3 = data + data2;   // string concatenation
 
-alert (data3);
+// alert (data3);
 
-console.log(data3);
+// console.log(data3);
+
+
+
+
+
+// number + number = addition
+// number + string =  concatenation
+//  string + number  =  concatenation
+//  string + string  =  concatenation
+
+// let data = "5";
+// let data2 = 5;
+
+// let data3 = data + data2;   // addtion
+
+// console.log(data3);
+
+
+
+let data = "Hello world";
+ let data2 = data.includes("H");  // data.include is used to check that number
+
+ console.log(data2);
+
+
+
+
+ let Data = "5";
+ let Data2 = 5;
+ console.log( Number(Data) + Data2);
+
+
+ // Math operators = + - * / %  .... is used math operators in js.
+
+
+ let x = 10;
+ let x1 = 3;
+ let result = x % x1;  // % is used to check the remaining values e.g 10 divided by 3 ,, the is 3 and 1 is remaining 
+ console.log (result); 
+
+
+ let userFavnumber = prompt("what your favourite number");
+ console.log(userFavnumber);
+
+ let userName = prompt("What is your name");
+ console.log("welcome to my website " + userName);
+ 
+ document.write("welcome to my website " + userName);  // document.write is used for write any thing on the web page
+
+ // To check what computer is select the number throgh themself
+
+ let randomNumber = Math.random();
+
+ console.log(randomNumber); // computer give only number 0-1 ,, and answer in dicimal
+
+ //The coding used for rounding off decimal numbers is "Math.round".
+
+ randomNumber = randomNumber * 10;
+
+ let roundedNumber = Math.round(randomNumber);
+console.log(roundedNumber);
+
+
+let num = 5 + 5 ** 3;  // ** double multiply is used for power of any number e,g (5**2)=25
+
+console.log(num);
+
+
+
+
+
+
+
